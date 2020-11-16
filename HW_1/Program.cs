9e7805dc7f0893ca8b2ba5ec6b594325cc1d0577
Sandra Gormakova <masha8933m@gmail.com>
2020-11-16 17:19:21 +0300

    new

--- HW_1/Program.cs
+++ HW_1/Program.cs
@@ -7,6 +7,7 @@ namespace HW_1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Привет, мир!");
         }
     }
 }
